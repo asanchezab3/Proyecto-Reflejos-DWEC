265db5ac44cd49aec551fcdb4b78eb4def960977
Aimar Sanchez <asanchezab3@educacion.navarra.es>
2024-03-19 21:44:46 +0100

    Diseño pantalla login hecho(Falta que verifique)

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import LoginComponent from "./LoginComponent";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Componente principal de la app
 function App() {
   return (
-    <h1>Prueba desde APP</h1>
+    <div className="container">
+      <LoginComponent />
+    </div>
   );
 }
-export default App
+export default App;
---
+++ src/Components/LoginComponent.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { db } from '../firebase/config'; 
+import { collection, getDocs } from 'firebase/firestore';
+
+const LoginComponent = () => {
+  const [formState, setFormState] = useState({
+    user: "",
+    password: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({ ...formState, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    console.log("The form was submitted: ", formState);
+    event.preventDefault();
+  };
+
+  return (
+    <form className="container mt-3" onSubmit={handleSubmit}>
+      <h1>LOGIN</h1>
+      <div className="mb-3">
+        <label htmlFor="user" className="form-label">
+          Usuario
+        </label>
+        <input
+          type="email"
+          className="form-control"
+          name="user"
+          id="user"
+          value={formState.user}
+          onChange={handleInputChange}
+          aria-describedby="userHelp"
+          required
+        />
+        <div id="userHelp" className="form-text">
+          Usuario para poder acceder a la aplicación. Formato de email.
+        </div>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="password" className="form-label">
+          Contraseña
+        </label>
+        <input
+          type="password"
+          className="form-control"
+          name="password"
+          id="password"
+          value={formState.password}
+          onChange={handleInputChange}
+          aria-describedby="passwordHelp"
+          required
+        />
+        <div id="passwordHelp" className="form-text">
+          Contraseña correspondiente al usuario para poder acceder a la
+          aplicación.
+        </div>
+      </div>
+      <div className="row">
+        <div className="col d-flex justify-content-end">
+          <button type="submit" className="btn btn-outline-success">
+            Entrar
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default LoginComponent;
